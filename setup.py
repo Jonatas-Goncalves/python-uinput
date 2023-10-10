1b78d376bf85857e8f8cd19c4925e90baeecd749
Jonatas Gonçalves <jonatasgoncalves@protonmail.com>
2023-10-10 14:25:30 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(name='python-uinput',
         "Programming Language :: Python :: 3.1",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.11",
         ],
       long_description="""
 Python-uinput is Python interface to Linux uinput kernel module which
